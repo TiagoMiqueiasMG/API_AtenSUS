e976e1391e7bdf569cbd94d3865bd4761dffde35
TiagoMiqueiasDesenvolvedor <tiagomiqueiasrodrigues@gmail.com>
2023-03-25 18:15:29 -0300

    Adicionar arquivos de projeto.

---
+++ API_AtenSUS/Models/Paciente.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_AtenSUS.Models
+{
+    [Table("Pacientes")]
+    public class Paciente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Telefone { get; set;}
+        [Required]
+        public string Endereco { get; set; }
+
+        [Required]
+        public int TriagemId { get; set; }
+
+        public Paciente Triagem { get; set; }
+
+
+
+    }
+}
---
+++ API_AtenSUS/Models/Triagem.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_AtenSUS.Models
+{
+    [Table("Triagens")]
+    public class Triagem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime DataHora { get; set; }
+        [Required]       
+        public int Nivel { get; set; }
+        [Required]
+
+        public TipoDeEspecialidade Tipo{ get; set; }
+
+        public ICollection<Paciente> Pacientes { get; set; }   
+    }
+
+    public enum TipoDeEspecialidade
+    {
+        Clinico_Geral,
+        Ortopedista,
+        Pediatra,
+        Cirurgiao
+    }
+}
